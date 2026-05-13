b478ee6b76a562c35252ae77b61a67e9b417b60e
RakimuAzad <qwertylikescake4ever@gmail.com>
2026-05-13 13:46:14 -0400

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -2,7 +2,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Silentheart Hub",
-    LoadingTitle = "Talking to Dreadstar...",
+    LoadingTitle = "Becoming Silent",
     LoadingSubtitle = "Stay safe.",
     ConfigurationSaving = {
         Enabled = false,
